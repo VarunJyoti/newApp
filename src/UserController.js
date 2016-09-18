5dbb57bfc656b6ae8dd0f4fa2fc971d92953b2fc
VarunJyoti <varunjyoti@Varuns-MacBook-Pro.local>
2016-09-18 22:22:16 +0530

    API integration user list

--- src/UserController.js
+++ src/UserController.js
@@ -1,13 +1,16 @@
-angular.module('myApp').controller("UserController", function($scope, $state, $stateParams){
-	$scope.rowCollection = [
-      {userId:"1", firstName: 'Laurent', lastName: 'sa@e.com', phone: "1234545", email: 'whatever@gmail.com'},
-      {userId:"2", firstName: 'Blandine', lastName: 'as@sa.com', phone: "5645635", email: 'oufblandou@gmail.com'},
-      {userId:"3", firstName: 'Francoise', lastName: 'hj@d.com', phone: "543543", email: 'raymondef@gmail.com'},
-      {userId:"4", firstName: 'Laurent', lastName: 'sa@e.com', phone: "5435646", email: 'whatever@gmail.com'},
-      {userId:"5", firstName: 'Blandine', lastName: 'as@sa.com', phone: "543534", email: 'oufblandou@gmail.com'},
-      {userId:"6", firstName: 'Francoise', lastName: 'hj@d.com', phone: "53453", email: 'raymondef@gmail.com'}
-  ];
-  if($stateParams.userId != ""){
+angular.module('myApp').controller("UserController", function($scope, $state, $stateParams, $http){
+
+  $http({
+    method: 'GET',
+    url: '/userlist'
+  }).then(function (response) {
+      $scope.rowCollection = response.data;
+    }, function (response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
+	
+  if($stateParams.userId){
     $scope.record = $scope.rowCollection.find(function(e){
         if(e.userId == $stateParams.userId){
           return true;
